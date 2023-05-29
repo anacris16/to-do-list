24c5c7f7fe4f592d52a68d2a831f84ccec172d7e
anacris16 <titialtuve1605@gmail.com>
2023-05-29 22:02:51 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <TodoWrapper />
+      <footer>
+        This project was coded by Ana Cristina Altuve and it is{" "}
+        <a href="https://github.com/anacris16/to-do-list">open-sourced</a>
+      </footer>
     </div>
   );
 }
